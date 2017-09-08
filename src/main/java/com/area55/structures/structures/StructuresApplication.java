b4d542fdddaec6a04e7f30e9b0ec01577894f99f
anishbabu <anish.bharatha@gmail.com>
2017-09-07 22:55:48 -0400

    project structure changed

---
+++ src/main/java/com/area55/structures/structures/StructuresApplication.java
@@ -0,0 +1,13 @@
+package com.area55.structures.structures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructuresApplication.class, args);
+		System.out.println("Hello, I am working!!!");
+	}
+}
---
+++ src/test/java/com/area55/structures/structures/StructuresApplicationTests.java
@@ -0,0 +1,16 @@
+package com.area55.structures.structures;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StructuresApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
